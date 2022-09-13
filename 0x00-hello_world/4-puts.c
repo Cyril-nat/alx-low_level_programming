9d862d9d8b22681d9d077e95e0471f465673cc87
Cyril Igberi <98932984+Cyril-nat@users.noreply.github.com>
2022-09-12 21:00:52 -0700

    Modified task 4-6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,11 @@
 
 /**
  * main - prints the phrase "Programming is like building a multilingual puzzle
- * Return - 0 (successful)
+ * Return: 0 if successful, non-zero if not
  */
 
-int main(void) 
+int main(void)
 {
-  
   puts("\"Programming is like building a multilingual puzzle");
   return (0);
-  
 }
