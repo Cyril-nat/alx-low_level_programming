8b5bc0efa67666e8329d0f2fae56cff254382cb2
Cyril Igberi <98932984+Cyril-nat@users.noreply.github.com>
2022-09-20 19:54:22 -0700

    Added tasks 4-9

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * followed by a new line
+ * @str: sample string
+ */
+
+void puts_half(char *str)
+{
+int i = 0;
+int j;
+
+while (str[i] != '\0')
+{
+i++;
+}
+
+if (i % 2 == 1)
+{
+j = (i - 1) / 2;
+j += 1;
+}
+
+else
+{
+j = i / 2;
+}
+
+for (; k < j; k++)
+{
+_putchar(str[k]);
+}
+_putchar('\n');
+}
