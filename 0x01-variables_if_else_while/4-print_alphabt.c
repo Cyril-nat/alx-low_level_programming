278ee86858f6044de797cce5e9f3b27de67dabad
Cyril Igberi <igberi.cyril@gmail.com>
2023-03-20 16:17:04 -0700

    added task 4 and 5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabets in lowercase with
+ * the exception of letter 'q' and 'e'.
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
