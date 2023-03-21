dc8077bb7b0a77f129e683378dc9c25802804250
Cyril Igberi <igberi.cyril@gmail.com>
2023-03-20 17:40:37 -0700

    completed task 6-9

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - program to print all numbers of base16 in lowercase.
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	for (c = 'a'; c < 'g'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
