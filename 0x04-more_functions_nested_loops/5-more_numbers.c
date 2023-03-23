8ff70f40ba8f59c61efa85fc9ab77f6f877a366a
Cyril Igberi <igberi.cyril@gmail.com>
2023-03-23 16:40:28 -0700

    corrected task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,28 +6,19 @@
  */
 
 void more_numbers(void)
-
 {
+	int i, j;
 
-int i, j;
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j > 9)
+			_putchar(j / 10 + '0');
+			else
+			_putchar(j % 10 + '0');
+		}
 
-for (i = 0; i < 10; i++)
-
-{
-
-for (j = 0; j < 15; j++)
-
-{
-
-if (j >= 10)
-_putchar(j % 10 + '1');
-else
-_putchar(j % 10 + '0');
-
-}
-
-_putchar('\n');
-
-}
-
+		_putchar('\n');
+	}
 }
