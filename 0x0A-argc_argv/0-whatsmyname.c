7000fde568b06acb8fffc2fa860da53afcf8ce28
Cyril Igberi <igberi.cyril@gmail.com>
2023-04-10 03:32:26 -0700

    corrected double variable declaration

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,11 +3,11 @@
 /**
  * main - function that prints the name of the file
  * @argc: count of command line arguments
- * @argv: is an array of the arguments
+ * @argv: is an array of pointers to the arguments
  * Return: Always 0 (Success)
  */
 
-int main(int argc, int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc - argc]);
 	return (0);
